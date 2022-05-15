7f14f64a0076727624b24a2638e74a77c32e026b
esthi <hoeuyu@student.ethz.ch>
2022-05-15 09:24:39 +0200

    Create index.py

---
+++ doks/template/index.py
@@ -0,0 +1,4 @@
+aws kendra create-index \
+ --name cli-getting-started-index \
+ --description "Index for CLI getting started guide." \
+ --role-arn arn:aws:iam::account id:role/KendraRoleForGettingStartedIndex
